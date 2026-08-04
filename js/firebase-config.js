ef454233fd95838822b6a552da4b451fbbba0526
engliterature0221-oss <engliterature0221@gmail.com>
2026-08-04 23:27:28 +0530

    file updates

---
+++ js/firebase-config.js
@@ -0,0 +1,20 @@
+
+// Firebase Configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyC_OQRVex2rH3tzBfDmaQzc-nPEjX5AsM0",
+
+  authDomain: "ekantra-befbc.firebaseapp.com",
+
+  projectId: "ekantra-befbc",
+
+  storageBucket: "ekantra-befbc.firebasestorage.app",
+
+  messagingSenderId: "421572188153",
+
+  appId: "1:421572188153:web:dd291a3962fe3ea0184f28",
+
+  measurementId: "G-68GZ777N80"
+
+};
--- success.js
+++ success.js
@@ -1,3 +1,6 @@
+/* ==========================================
+        SUCCESS PAGE
+========================================== */
 
 const params = new URLSearchParams(window.location.search);
 
@@ -7,21 +10,34 @@ const product = PRODUCTS[productId];
 
 if(product){
 
-document.getElementById("success-product").innerText = product.name;
+    document.getElementById("success-product").innerText =
+    product.name;
 
-document.getElementById("download-btn").href = product.pdf;
+    document.getElementById("download-btn").href =
+    product.pdf;
 
 }
 
-/* ======================================
-        ORDER DETAILS
-====================================== */
+/* ==========================================
+        ORDER ID
+========================================== */
 
-const orderId = "EK-" + Date.now();
+const now = new Date();
 
-document.getElementById("order-id").innerText = orderId;
+const orderId =
+"EK-" +
+now.getFullYear() +
+(now.getMonth()+1).toString().padStart(2,"0") +
+now.getDate().toString().padStart(2,"0") +
+"-" +
+Math.floor(Math.random()*9000+1000);
 
-const today = new Date();
+document.getElementById("order-id").innerText =
+orderId;
+
+/* ==========================================
+        PURCHASE DATE
+========================================== */
 
 document.getElementById("purchase-date").innerText =
-today.toLocaleDateString();
+now.toLocaleDateString();
